a06dfe75547042e1ce1f8fbb6b2a3ee609a0a623
ome chukwuemeka <omeco4christ@gmail.com>
2022-02-14 15:46:48 +0000

    2nd commit

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const express = require('express');
+// server.js
+const express = require("express");
 const app = express();
- 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
+const server = require("http").Server(app);
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
 });
- 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+server.listen(3030);
